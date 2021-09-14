7b33c5b93db0db9bd6be93fe62336820e6a3096b
Hyewonjang <hyewon5254295@gmail.com>
2021-09-14 22:23:50 +0900

    django course 17 commit

--- Accountapp/views.py
+++ Accountapp/views.py
@@ -2,11 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from Accountapp.models import HelloWorld
+
 
 def hello_world(request):
     #return HttpResponse('Hello world!')  # HttpResponse는 views에서 직접적으로 response해주는 것.
 
     if request.method == "POST":
-        return render(request, 'Accountapp/hello_world.html', context={'text': 'POST METHOD!!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()  # 새로운 HelloWorld 객체 생성
+        new_hello_world.text = temp    # 새로운 HelloWorld 객체의 text 속성 작성
+        new_hello_world.save()    # 새로운 HelloWorld 객체 DB에 저장
+
+        return render(request, 'Accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'Accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
